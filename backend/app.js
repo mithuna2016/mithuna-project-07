6c447ac66b28e27f2c7b04154d1d59207e4cdc9f
mithuna <ais_mini@yahoo.com>
2021-02-28 15:45:30 +0000

    set the frontend

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+//mongodb+srv://mithunap-6:<password>@cluster0.jpm7k.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const userRoutes = require('./routes/user');
+const { response } = require("express");
+const path = require('path');
+const app = express();
+
+// connect with mongoose
+
+mongoose.connect('mongodb+srv://mithunap-6:Sarasvin23@cluster0.jpm7k.mongodb.net/project-7?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('successfully connected to mongo db atlass')
+})
+.catch((erro)=>{
+    console.log('enable to connect to mongo db');
+});
+
+
+//to prevent CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  router.get('/', async (req, res) => {
+    
+    res.send("hello");
+  });
+
+
+  module.exports=app;
---
+++ backend/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+userId:{ type: String},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName:{type: String, required: true},
+  lastName:{type: String, required: true}
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
